87f572e6948df0e96b1b01ce3980ca44b72dc049
Devansh Kaushik <devanshk9125@gmail.com>
2025-09-16 19:38:27 +0530

    implement login-logout auth routes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import {ENV} from "../lib/env.js";
+
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
     try {
@@ -33,12 +34,6 @@ export const signup = async (req, res) => {
         });
     
         if (newUser) {
-          // before CodeRabbit:
-          // generateToken(newUser._id, res);
-          // await newUser.save();
-    
-          // after CodeRabbit:
-          // Persist user first, then issue auth cookie
           const savedUser = await newUser.save();
           generateToken(savedUser._id, res);
     
@@ -62,4 +57,38 @@ export const signup = async (req, res) => {
     }
 
 
-}
+};
+
+export const login = async (req, res) => {  
+      const { email, password } = req.body;
+
+    if (!email || !password) { 
+      return res.status(400).json({ message: "Email and password are required" });
+    }
+
+    try {
+      const user = await User.findOne({ email });
+      if (!user) return res.status(400).json({ message: "Invalid credentials" });
+      // never tell the client which one is incorrect: password or email
+
+      const isPasswordCorrect = await bcrypt.compare(password, user.password);
+      if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+
+      generateToken(user._id, res);
+
+      res.status(200).json({
+        _id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        profilePic: user.profilePic,
+      });
+    } catch (error) {
+      console.error("Error in login controller:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const logout = (_, res) => {
+  res.cookie("jwt", "", { maxAge: 0 });
+  res.status(200).json({ message: "Logged out successfully" });
+};
