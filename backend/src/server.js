60e921f38b37314acb19748876c319ae801a123e
Devansh Kaushik <devanshk9125@gmail.com>
2025-09-17 00:00:22 +0530

    implement update profile

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ import messageRoutes from "./routes/message.route.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
 import {ENV} from "./lib/env.js";
+import cookieParser from "cookie-parser";
+
 
 const app = express();
 const __dirname = path.resolve();
@@ -11,6 +13,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages",messageRoutes); 
